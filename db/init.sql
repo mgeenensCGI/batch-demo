e0910e91eec5fce080a3adc8a3a40bbb87df3754
GEENENS <maxime.geenens@cgi.com>
2026-06-12 15:12:51 +0200

    Docker and DB config

---
+++ db/init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS customers (
+                                         id BIGINT PRIMARY KEY,
+                                         first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    city VARCHAR(100) NOT NULL,
+    created_at DATE NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS customer_import_error (
+                                                     id BIGSERIAL PRIMARY KEY,
+                                                     raw_line TEXT NOT NULL,
+                                                     error_message TEXT NOT NULL,
+                                                     created_at TIMESTAMP NOT NULL DEFAULT NOW()
+    );
